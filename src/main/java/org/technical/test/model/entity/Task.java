8878d1348f8399a21c1becde6fabcc4a3ec794e3
GeraldFMoraMora <geraldmoramora@gmail.com>
2023-12-20 18:46:37 -0600

    Agregado lombok y entidades al proyecto

--- src/main/java/org/technical/test/model/entity/Task.java
+++ src/main/java/org/technical/test/model/entity/Task.java
@@ -1,5 +1,20 @@
 package org.technical.test.model.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Task {
-    
+
+    public Integer id;
+    public String description;
+    public String state;
+    public String imageUrl;
+    public boolean isActive;
+
+    public Task(){
+        this.isActive = true;
+    }
+
 }
--- src/main/java/org/technical/test/model/entity/User.java
+++ src/main/java/org/technical/test/model/entity/User.java
@@ -1,5 +1,22 @@
 package org.technical.test.model.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class User {
+
+    public Integer id;
+    public String name;
+    public String password;
+    public Integer telephone;
+    public String ageRange;
+    public Integer gender;
+    public boolean isActive;
+
+    public User(){
+        this.isActive = true;
+    }
     
 }
